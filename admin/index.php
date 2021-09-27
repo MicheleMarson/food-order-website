3c12fe5868bfbf14f060a1a83978a5666970a6eb
Mihaela Marson <mitchmarson666@gmail.com>
2021-09-27 13:05:14 +0200

    adding files

---
+++ admin/index.php
@@ -0,0 +1,55 @@
+<html>
+<head>
+  <title>Food order Website - Home Page</title>
+  <link rel="stylesheet" href="../css/admin.css">
+</head>
+<body>
+  <!-- menu section starts -->
+  <div class="menu text-center">
+    <div class="wrapper">
+      <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Admin</a></li>
+        <li><a href="#">Category</a></li>
+        <li><a href="#">Food</a></li>
+        <li><a href="#">Order</a></li>
+      </ul>
+    </div>
+  </div>
+  <!-- menu section ends -->
+
+  <!-- main content section starts -->
+  <div class="main">
+    <div class="wrapper">
+        <h1>Dashboard</h1>
+
+        <div class="col-4 text-center">
+          <h1>5</h1>
+          Categories
+        </div>
+        <div class="col-4 text-center">
+          <h1>5</h1>
+          Categories
+        </div>
+        <div class="col-4 text-center">
+          <h1>5</h1>
+          Categories
+        </div>
+        <div class="col-4 text-center">
+          <h1>5</h1>
+          Categories
+        </div>
+        <div class="clearfix"></div>
+    </div>
+  </div>
+  <!-- main content section ends -->
+
+  <!-- footer section starts -->
+  <div class="footer">
+    <div class="wrapper">
+      <p class="text-center">2021 All right reserved, Restaurant. Developed By <a href="#">Mihaela Marson</a></p>
+    </div>
+  </div>
+  <!-- footer section ends -->
+</body>
+</html>
